c6bcf553ed54541f06b8d851c0fb6011c4d72542
Michael Cuison <xurpas7@gmail.com>
2022-10-21 10:21:22 +0800

    mac20221021

--- src/app/LetMeInn.java
+++ src/app/LetMeInn.java
@@ -6,6 +6,17 @@ import org.rmj.appdriver.GRider;
 
 public class LetMeInn {
     public static void main(String [] args){                
+        String path;
+        
+        if(System.getProperty("os.name").toLowerCase().contains("win")){
+            path = "D:/GGC_Java_Systems";
+        }
+        else{
+            path = "/srv/GGC_Java_Systems";
+        }
+        
+        System.setProperty("sys.default.path.config", path); 
+        
         GRider oApp = new GRider();
         
         if (!oApp.loadEnv("gRider")) {
